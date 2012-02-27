158844126600e809655b9950418bdf5a83982fe4
Ben Scholz <bbscholz@gmail.com>
2012-02-27 00:39:08 -0600

    added beautify.js for working pretty print

--- src/pretty.js
+++ src/pretty.js
@@ -2,30 +2,8 @@
  * _pretty: string -> string
  *
  * Pretty print Javascript code.
- * This is largely incorrect.
+ * Uses beaufity.js: https://github.com/einars/js-beautify
  */
 var _pretty = function (source) {
-
-	var pretty_print = function (s, tabs) {
-		var i, t = tabs + "    ";
-		var code = ""
-		for (i = 0; i < s.length; i++) {
-			switch (s[i]) {
-				case "{":
-					code += s[i] + "\n" + t;
-					break;
-				case "}":
-					code += tabs + s[i];
-					break;
-				case ";":
-					code += s[i] + "\n" + tabs;
-					break;
-				default:
-					code += s[i];
-			}	
-		}
-		return code;
-	};
-
-	return pretty_print(source, "");
+	return js_beautify(source);
 };
